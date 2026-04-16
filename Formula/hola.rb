5eb9066a915f35f16ad8b140ec7fde88f64321d8
ratazzi <ratazzi.potts@gmail.com>
2026-04-16 23:23:57 +0800

    hola 0.3.0

--- Formula/hola.rb
+++ Formula/hola.rb
@@ -1,13 +1,13 @@
 class Hola < Formula
   desc "Single-binary Mac setup tool - Brewfile + mise.toml + dotfiles"
   homepage "https://github.com/ratazzi/hola"
-  version "0.1.0"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/ratazzi/hola/releases/download/v#{version}/hola-macos-aarch64"
-      sha256 "e6b6733408cc29bbf78a2d7179b2d9c9c3fb25dd41f49607f1474acdae694d56"
+      sha256 "6a58126d39e5376832e9d569c71802b7e5164ca2ed3687a981a66ff9f7e0f933"
     end
   end
 
